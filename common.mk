85df34aff27fc1121268285d19c05dec6913c030
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-06-19 16:45:32 +0200

    sm8350-common: Time for tequila!

--- common.mk
+++ common.mk
@@ -373,7 +373,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-tequila
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
