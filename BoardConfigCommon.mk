5a732b0859b5c4312e8b6aff9c70aa7ec1593d2d
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-10-29 23:48:26 +0100

    sm8350-common: Include common qcom compatibility matrix
    Change-Id: I9a165b487cb8a81d18c0b6dd01a1deea0374fce7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/tequila/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
